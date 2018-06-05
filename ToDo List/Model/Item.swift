4debfed7e99eff13e18a1cd2c2b69855d8554ead
Irina <zhogova.irina1@yandex.ru>
2018-06-05 18:02:45 +0300

    MVC

---
+++ ToDo List/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDo List
+//
+//  Created by Ирина Жогова on 05.06.2018.
+//  Copyright © 2018 Irina Zhogova. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title = ""
+    var done = false
+    
+}
